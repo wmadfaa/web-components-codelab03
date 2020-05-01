df09802461bfece0732484fe1f7f06fce2f231d9
Wasim Almadfaa <wm.madfaa@gmail.com>
2020-05-01 18:15:42 +0000

    Displaying a loading state

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,9 @@ export default class MyElement extends LitElement {
   @property({ type: Array })
   breweries = [];
 
+  @property({ type: Boolean })
+  loading = true;
+
   connectedCallback() {
     super.connectedCallback();
 
@@ -16,12 +19,16 @@ export default class MyElement extends LitElement {
   }
 
   async _fetchBreweries() {
+    this.loading = true;
+    // await new Promise((res) => setTimeout(res, 1000)); // uncomment to test loading state
     const res = await fetch(API);
     const jsonRes = await res.json();
     this.breweries = jsonRes;
+    this.loading = false;
   }
 
   render() {
+    if (this.loading) return html`<p>loading...</p>`;
     return html` <pre>${JSON.stringify(this.breweries, null, 2)}</pre> `;
   }
 }
